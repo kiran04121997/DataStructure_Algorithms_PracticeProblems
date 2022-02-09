95011eb5f8c6d8d032d9ee68e09ce3a312b457ff
kiran04121997 <bhosalekiran259@gmail.com>
2022-02-09 13:12:54 +0530

    [kiran] DataStructureAndAlgorithms

---
+++ DataStructure_Algorithms_Problems/DataStructure_Algorithms_Problems/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure_Algorithms_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DataStructureandAlgorithms");
+            Console.ReadLine();
+        }
+    }
+}
